78176e18fa9f65d96f2985ab412900067d329b3f
chendaijun <dj1479109164@163.com>
2023-03-23 16:19:00 +0000

    1.支持ggtagsmode自动切换；2.支持M-w C-y复制

---
+++ lisp/init-local.el
@@ -0,0 +1,26 @@
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1))))
+
+(setq x-select-enable-clipboard t)
+(defun copy-to-clipboard ()
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+        (message "Yanked region to x-clipboard and clipboard!")
+        (call-interactively 'clipboard-kill-ring-save)
+        (shell-command-on-region (region-beginning) (region-end) "xclip -i -selection clipboard")
+        )
+    (if (region-active-p)
+        (progn
+          (call-interactively 'clipboard-kill-ring-save)
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          (message "Yanked region to x-clipboard and clipboard!")
+          (deactivate-mark))
+      (message "No region active; can't yank to clipboard!")))
+  )
+
+(global-set-key (kbd "M-w") 'copy-to-clipboard)
+
+(provide 'init-local)
